4e75847d7d33bddd9900dcb42bfe3d1e4cdd1b16
isragosterit <isra.gstrt@gmail.com>
2023-02-27 00:12:59 +0300

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_project.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace web_project.Controllers
+{
+    public class LoginController : Controller
+    {
+        DbModels dbModel = new DbModels();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User log)
+        {
+            var user = dbModel.User.Where(x => x.email == log.email && x.password == log.password).Count();
+            if (user > 0)
+            {
+                ViewBag.SuccessMessage = "Login is successfull.";
+                return View();
+            }
+            else
+            {
+                ViewBag.DuplicateMessage = "Check your e-mail or password!";
+                return View();
+            }
+
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_project.Controllers;
+using web_project.Models;
+
+namespace web_project.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                if (dbModel.User.Any(x => x.email == userModel.email))
+                {
+                    ViewBag.DuplicateMessage = "E-mail already exists.";
+                    return View("AddOrEdit", userModel);
+                }
+                dbModel.User.Add(userModel);
+                dbModel.SaveChanges();
+            }
+
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit", new User());
+        }
+    }
+}
